7071b604117b518d458e902ca214b7bf24b5f42b
Raz Elbaz <93310416+RazElbaz@users.noreply.github.com>
2022-06-04 17:42:51 +0300

    Add files via upload

--- sources/Schedule.cpp
+++ sources/Schedule.cpp
@@ -3,17 +3,21 @@
 namespace ariel {
     Schedule::Schedule(League &other):Round(1) {
         this->Tournament = other.getTeams();
+        //create two vectors for enter the teams to new game
         vector<Team> group1;
         vector<Team> group2;
+        //Counter for inserting teams into the game
         size_t counterGame=0;
         for (size_t i = 1; i < GroupSize_max; i++) {
             cout << "Round: " << this->Round++ << endl;
             for (size_t j = 0; j < GroupSize_max / 2; j++) {
+                //Inserting the top line in print
                 cout << this->Tournament.at(j).name() << "\t";
                 group1.push_back(Tournament.at(j));
             }
             cout << "\n" << endl;
             for (size_t k = GroupSize_max - 1; k >= (GroupSize_max / 2); k--) {
+                //Inserting the bottom line in print
                 cout << this->Tournament.at(k).name() << "\t";
                 group2.push_back(Tournament.at(k));
             }
@@ -23,8 +27,8 @@ namespace ariel {
                 this->Tournament.at(j) = this->Tournament.at(j - 1);
             }
             this->Tournament.at(1) = tmp;
-
         }
+        //adding the groups to the game
         for (size_t i = 0; i <group1.size() ; ++i) {
             Game current {group1.at(counterGame), group2.at(counterGame)};
             current.GameResult();
